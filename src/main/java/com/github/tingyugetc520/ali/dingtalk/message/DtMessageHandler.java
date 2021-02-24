ce745c76ba9f8d4c645a65e751c738df32745e54
tingyugetc520 <1364140672@qq.com>
2021-02-24 11:20:30 +0800

    添加注释

--- src/main/java/com/github/tingyugetc520/ali/dingtalk/message/DtMessageHandler.java
+++ src/main/java/com/github/tingyugetc520/ali/dingtalk/message/DtMessageHandler.java
@@ -15,7 +15,7 @@ public interface DtMessageHandler {
      * 处理消息，当消息处理成功时可以返回true，是被返回false
      * 未处理消息则可以返回null，比如仅仅是记录了一下日志而已
      *  此时不论是返回false还是true都会有问题，返回true则会认为消息处理成功返回false则会认为消息处理失败，然而消息实际未处理
-     *  此时返回null，返回钉钉失败的响应，在后续业务方可以拉取失败消息来重新处理
+     *  此时返回null，但依然会返回钉钉失败的响应，在后续业务方可以拉取失败消息来重新处理
      *
      * @param message      the message
      * @param context      上下文，如果handler或interceptor之间有信息要传递，可以用这个
